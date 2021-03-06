7dfc7fed472a8b53d468f3a5499ae9754c570c30
Tamas Galffy <ezittgtx@gmail.com>
2021-03-06 15:10:55 +0100

    chore: Generate Javadocs

--- src/main/java/com/github/elementbound/jamtracer/Jamtracer.java
+++ src/main/java/com/github/elementbound/jamtracer/Jamtracer.java
@@ -1,5 +1,8 @@
 package com.github.elementbound.jamtracer;
 
+/**
+ * Jamtracer application entry point.
+ */
 public class Jamtracer {
     public static void main(String[] args) {
         System.out.println("Jamtracer!");
