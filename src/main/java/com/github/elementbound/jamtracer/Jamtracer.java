8fc7c86c1f2a12c35bcabf434d9cfca9c3604fed
Tamas Galffy <ezittgtx@gmail.com>
2021-03-20 18:46:36 +0100

    refactor: Move raytracing loop to its own class

--- src/main/java/com/github/elementbound/jamtracer/Jamtracer.java
+++ src/main/java/com/github/elementbound/jamtracer/Jamtracer.java
@@ -1,16 +1,15 @@
 package com.github.elementbound.jamtracer;
 
-import com.github.elementbound.jamtracer.core.Color;
 import com.github.elementbound.jamtracer.core.Vector;
 import com.github.elementbound.jamtracer.display.AWTWindowDisplay;
 import com.github.elementbound.jamtracer.display.Display;
+import com.github.elementbound.jamtracer.raytracing.Raytracer;
 import com.github.elementbound.jamtracer.raytracing.camera.PerspectiveCamera;
 import com.github.elementbound.jamtracer.raytracing.shape.CubeShape;
 import com.github.elementbound.jamtracer.raytracing.shape.Shape;
 import com.github.elementbound.jamtracer.raytracing.shape.SphereShape;
 import com.github.elementbound.jamtracer.raytracing.shape.scene.Scene;
 import com.github.elementbound.jamtracer.raytracing.shape.scene.SimpleScene;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -25,9 +24,7 @@ public class Jamtracer {
    *
    * @param args CLI params
    */
-  public static void main(String[] args) throws InterruptedException {
-    System.out.println("Jamtracer!");
-
+  public static void main(String[] args) {
     Display display = new AWTWindowDisplay(WIDTH, HEIGHT);
     display.onClose().subscribe(v -> System.exit(0));
 
@@ -35,34 +32,12 @@ public class Jamtracer {
     camera.setAspectRatio(WIDTH, HEIGHT);
     camera.setFieldOfView(60.0);
 
-    Scene scene = new SimpleScene();
+    Scene scene = createScene();
 
-    Shape floor = new CubeShape();
-    floor.getTransform().update()
-            .setScale(new Vector(4.0, .25, 1.0))
-            .setPosition(new Vector(0.0, 0.0, -2.0))
-            .done();
-
-    scene.addShape(floor);
-
-    Stream.of(-1.0, 0.0, 1.0)
-            .map(i -> {
-              var sphere = new SphereShape();
-              sphere.getTransform().update()
-                      .setPosition(Vector.RIGHT.scale(i))
-                      .done();
-              return sphere;
-            }).forEach(scene::addShape);
-
-    Shape sphere = new SphereShape();
-    scene.addShape(sphere);
-
-    System.out.println("Starting render loop!");
+    Raytracer raytracer = new Raytracer();
 
     var yaw = 0.0;
     var pitch = -45.0;
-
-    scene.prepare();
 
     while (display.isOpen()) {
       yaw += 12.0;
@@ -79,45 +54,36 @@ public class Jamtracer {
               .setPosition(backward)
               .done();
 
-      IntStream.range(0, display.getWidth() * display.getHeight())
-              .parallel()
-              .map(i -> (97 + i * 79) % (display.getWidth() * display.getHeight()))
-              .mapToObj(i -> new Vector(i % display.getWidth(), i / display.getWidth()))
-              .forEach(screenCoords -> {
-                int x = (int) screenCoords.get(0);
-                int y = (int) screenCoords.get(1);
-
-                double u = (double) x / display.getWidth();
-                double v = (double) y / display.getHeight();
-
-                var ray = camera.getRay(new Vector(u, v));
-                var direction = ray.getDirection();
-
-                display.setPixel(x, y,
-                        new Color(direction.get(0), direction.get(1), direction.get(2)));
-
-                var hitResult = scene.raycast(ray);
-                if (hitResult.isHit()) {
-                  var normal = hitResult.normal();
-                  var texcoords = hitResult.texcoords();
-                  var point = hitResult.point().map(Math::abs).scale(1.0 / 8.0);
-                  var df = hitResult.distance() / 32.0;
-
-                  // display.setPixel(x, y, new Color(normal.get(0), normal.get(1), normal.get(2)));
-                  // display.setPixel(x, y, new Color(texcoords.get(0), texcoords.get(1), 0.0));
-                  // display.setPixel(x, y, new Color(df, df, df));
-                  display.setPixel(x, y, new Color(point.get(0), point.get(1), point.get(2)));
-                }
-
-                /*
-                if ((x + y * display.getWidth()) % 1024 == 0)
-                  display.present();
-                 */
-              });
-
+      raytracer.render(scene, camera, display);
       display.present();
-
-      System.out.println(camera.getTransform().getRotation());
     }
+  }
+
+  private static Scene createScene() {
+    Scene scene = new SimpleScene();
+
+    Shape floor = new CubeShape();
+    floor.getTransform().update()
+            .setScale(new Vector(4.0, 4.0, 1.0))
+            .setPosition(new Vector(0.0, 0.0, -1.0))
+            .done();
+
+    scene.addShape(floor);
+
+    Stream.of(-1.0, 0.0, 1.0)
+            .map(i -> {
+              var sphere = new SphereShape();
+              sphere.getTransform().update()
+                      .setPosition(Vector.RIGHT.scale(i))
+                      .done();
+              return sphere;
+            }).forEach(scene::addShape);
+
+    Shape sphere = new SphereShape();
+    scene.addShape(sphere);
+
+    scene.prepare();
+
+    return scene;
   }
 }
---
+++ src/main/java/com/github/elementbound/jamtracer/raytracing/Raytracer.java
@@ -0,0 +1,62 @@
+package com.github.elementbound.jamtracer.raytracing;
+
+import com.github.elementbound.jamtracer.core.Color;
+import com.github.elementbound.jamtracer.core.Vector;
+import com.github.elementbound.jamtracer.display.Display;
+import com.github.elementbound.jamtracer.raytracing.camera.Camera;
+import com.github.elementbound.jamtracer.raytracing.shape.scene.Scene;
+import java.awt.Point;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Raytracer implementation.
+ */
+public class Raytracer {
+  private Stream<Point> pixelStream(int width, int height) {
+    int pixelCount = width * height;
+    return IntStream.range(0, pixelCount)
+            .parallel()
+            .map(i -> (79 + i * 97) % pixelCount)
+            .mapToObj(i -> new Point(i % width, i / width));
+  }
+
+  /**
+   * Render scene viewed from camera to a display.
+   *
+   * @param scene   scene
+   * @param camera  camera
+   * @param display output display
+   */
+  public void render(Scene scene, Camera camera, Display display) {
+    pixelStream(display.getWidth(), display.getHeight())
+            .forEach(screenCoords -> {
+              var ray = getRayForPixel(screenCoords, display, camera);
+              var direction = ray.getDirection().map(d -> (1.0 + d) / 2.0);
+
+              var resultColor = new Color(direction.get(0), direction.get(1), direction.get(2));
+
+              var hitResult = scene.raycast(ray);
+              if (hitResult.isHit()) {
+                var normal = hitResult.normal();
+                var texcoords = hitResult.texcoords();
+                var point = hitResult.point().map(Math::abs).scale(1.0 / 8.0);
+                var df = hitResult.distance() / 16.0;
+
+                resultColor = new Color(normal.get(0), normal.get(1), normal.get(2));
+                // resultColor = new Color(texcoords.get(0), texcoords.get(1), 0.0);
+                // resultColor = new Color(df, df, df);
+                // resultColor = new Color(point.get(0), point.get(1), point.get(2));
+              }
+
+              display.setPixel(screenCoords.x, screenCoords.y, resultColor);
+            });
+  }
+
+  private Ray getRayForPixel(Point screenCoords, Display display, Camera camera) {
+    double u = (double) screenCoords.x / display.getWidth();
+    double v = (double) screenCoords.y / display.getHeight();
+
+    return camera.getRay(new Vector(u, v));
+  }
+}
